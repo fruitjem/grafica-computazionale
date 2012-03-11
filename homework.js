169a88dc6c8de42387409a48995316255382bb46
Federico Calvigioni <fcalvigioni@gmail.com>
2012-03-11 19:48:46 +0100

    funzioni che modellano un punto

--- homework.js
+++ homework.js
@@ -1,38 +1,65 @@
+var sqrt = Math.sqrt;
+var pow = Math.pow;
+var random = Math.random;
 
-function Point (x,y){
-	this.x = x;
-	this.y = y;
+/* POINT */
+
+var Point = function (x, y) {
+  this.x = x || 0;
+  this.y = y || 0;
 }
 
-Point.prototype.getDistance = function (point){
-	var distance;
-	
-	distance = Math.sqrt( Math.pow((this.x - point.x) , 2) + Math.pow( (this.y - point.y) ,2));
-    return distance; 
+Point.prototype.getDistance = function(point) {
+  return sqrt(pow(point.y - this.y, 2) + pow(point.x - this.y, 2));
+};
+
+Point.prototype.translate = function(dx, dy) {
+  this.x += dx;
+  this.y += dy;
+
+  return this;
+};
+
+
+/* TRIANGLE */
+
+var Triangle = function (p1, p2, p3) {
+  this.p1 = p1;
+  this.p2 = p2;
+  this.p3 = p3;
+
+  this.l1 = p2.getDistance(p3);
+  this.l2 = p3.getDistance(p1);
+  this.l3 = p1.getDistance(p2);
 }
 
-function Triangle (p1,p2,p3){
-	this.p1 = p1;
-	this.p2 = p2;
-	this.p3 = p3;
+Triangle.prototype.getPerimeter = function() {
+  return this.l1 + this.l2 + this.l3;
+};
 
-	this.getPerimeter = function() {
-		var lato1 = this.p1.getDistance(p2); 
-		var lato2 = this.p1.getDistance(p3);
-		var lato3 = this.p2.getDistance(p3);
-      	var perimeter;
-      	return perimeter = lato1 + lato2 + lato3;
-    }
+Triangle.prototype.getArea = function() {
+  var p = this.getPerimeter() / 2;
 
-    this.getArea = function () {
-    	 
-        var lato1 = this.p1.getDistance(p2); 
-		var lato2 = this.p1.getDistance(p3);
-		var lato3 = this.p2.getDistance(p3);
-		var area;
-		var p = ( this.getPerimeter()/2 ) ;
-		//Formula di ERONE
-		// A = |p (p - l1) (p - l2) (p - l3)|
-		return area = Math.sqrt(p * (p - lato1) * (p - lato2) * (p - lato3));
-	}
+  return sqrt(p*(p - this.l1)*(p - this.l2)*(p - this.l3));
+};
+
+
+var randomPoint = function () {
+  var x1 = random() * 200 - 100;
+  var y1 = random() * 200 - 100;
+  
+  return new Point(x1, y1);
+};
+
+var randomPoints = function (n) {
+  var n = n || 1;
+  var res = new Array(n);
+
+  for (var i = 0; i < n ; i += 1) {
+    res[i] = randomPoint();
+  }
+
+  return res;
 }
+
+var points = randomPoints(100);
